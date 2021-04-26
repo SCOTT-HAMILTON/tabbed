03ea79c3b25f290bc7bbd0f2abb8c57bc06e67a1
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2021-04-26 19:29:06 +0200

    Cleaning ci test files

--- coverage/cpu-checker/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  pkgs ? import (builtins.fetchTarball {
-    url = "http://github.com/NixOS/nixpkgs/archive/d395190b24b27a65588f4539c423d9807ad8d4e7.tar.gz";
-    sha256 = "0r1kj8gf97z9ydh36vmgrar1q4l9ggaqiygxjvp8jmr1948y0nh2";
-  }) {}
-}:
-with pkgs; callPackage ./cpu-checker.nix {}
